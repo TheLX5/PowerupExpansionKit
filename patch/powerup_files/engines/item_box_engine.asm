82c4c6dad8812adf675810e9ff21b5e786f74604
TheLX5 <luisyuregi@gmail.com>
2022-04-20 21:14:40 -0700

    implemented item box

--- patch/powerup_files/engines/item_box_engine.asm
+++ patch/powerup_files/engines/item_box_engine.asm
@@ -2,11 +2,14 @@ pushpc
 
     org $009079
         item_box_write_hijack:
-            %invoke_sa1_powerups(item_box_write)
+            if !disable_item_box == !no
+                %invoke_sa1(item_box_write)
+            endif
             rts 
 
 pullpc 
 
+if !disable_item_box == !no
     item_box_write: 
         ldy #$E0 
         bit $0D9B|!addr
@@ -20,8 +23,12 @@ pullpc
     .no_special
         ldx $0DC2|!addr
         beq .return
+        lda.l item_box_page-1,x
+        xba 
         lda.l item_box_graphics-1,x
         jsl find_and_queue_gfx
+        bcc .return
+        ldx $0DC2|!addr
         lda !dss_tile_buffer+$00
         sta $0202|!addr,y
         lda.l item_box_props-1,x
@@ -35,9 +42,9 @@ pullpc
     .not_star
         ora #$31
         sta $0203|!addr,y
-        lda #$78
+        lda #!item_box_item_x_pos
         sta $0200|!addr,y
-        lda #$0F
+        lda #!item_box_item_y_pos
         sta $0201|!addr,y
         tya 
         lsr #2
@@ -47,13 +54,60 @@ pullpc
     .return
         rtl 
     
-        item_box_graphics:
-            db !dss_id_mushroom
-            db !dss_id_fire_flower
-            db !dss_id_star
-            db !dss_id_feather
-        
-        item_box_props:
-            db $08,$0A,$00,$04
-        item_box_star_props:
-            db $00,$02,$04,$02
+    item_box_graphics:
+    !i #= 0
+    while !i < !max_item_num
+        %internal_number_to_string(!i)
+        if not(stringsequal("!{item_!{_num}_path}", "0"))
+            db !{item_!{_num}_dss_id}
+        else
+            db $FF
+        endif
+        !i #= !i+1
+    endif
+
+    item_box_page:
+    !i #= 0
+    while !i < !max_item_num
+        %internal_number_to_string(!i)
+        if not(stringsequal("!{item_!{_num}_path}", "0"))
+            db !{item_!{_num}_dss_page}
+        else
+            db $FF
+        endif
+        !i #= !i+1
+    endif
+
+    item_box_props:
+        !i #= 0
+        while !i < !max_item_num
+            %internal_number_to_string(!i)
+            if not(stringsequal("!{item_!{_num}_path}", "0"))
+                db !{item_!{_num}_sprite_prop}
+            else
+                db $FF
+            endif
+            !i #= !i+1
+        endif
+
+    item_box_star_props:
+        db $00,$02,$04,$02
+
+endif
+
+find_and_queue_gfx:
+    pha 
+    xba
+    and #$01
+    beq +
+    lda #$03
++   
+    tax 
+    lda.l $01DF78+3+2,x
+    sta $00
+    lda.l $01DF78+3+2+1,x
+    sta $01
+    lda.l $01DF78+3+2+2,x
+    sta $02
+    pla 
+    jml [!dp]
