07aa5534b20b4a5c169ba9542edb6008332b0747
TheLX5 <luisyuregi@gmail.com>
2022-09-19 15:24:31 -0700

    fixes tilemap index not being properly updated on player powerdown

--- patch/powerup_files/engines/powerdown_engine.asm
+++ patch/powerup_files/engines/powerdown_engine.asm
@@ -12,12 +12,13 @@ macro powerdown_shrink(power_num, sfx_num, sfx_port)
     lda.b #<sfx_num>
     sta.w <sfx_port>|!addr
     lda #$2F
-    sta $1496|!addr
+    sta !player_animation_timer
     sta $9D
     lda #$01
     sta $71
     lda.b #<power_num>
-    sta $19
+    sta !player_powerup
+    jsr redraw_player
 endmacro
 
 macro powerdown_smoke(power_num, sfx_num, sfx_port)
@@ -26,7 +27,7 @@ macro powerdown_smoke(power_num, sfx_num, sfx_port)
     jsl $01C5AE|!bank
     inc $9D
     lda.b #<power_num>
-    sta $19
+    sta !player_powerup
     jsr hide_mario
 endmacro
 
@@ -42,7 +43,8 @@ macro powerdown_palette(power_num, sfx_num, sfx_port)
     lda #$7F
     sta $1497|!addr
     lda.b #<power_num>
-    sta $19
+    sta !player_powerup
+    jsr redraw_player
 endmacro
 
 reset bytes
@@ -54,6 +56,13 @@ hide_mario:
     jsl $00E2BD
     ply
     rts
+
+redraw_player:
+    phy
+    jsl $00E2BD
+    ply
+    rts
+
 
 powerdown:
     phx
