56b3206cc2f945f0c139acb82f572bf8851d7fd0
TheLX5 <luisyuregi@gmail.com>
2023-12-29 00:48:45 -0700

    Removed a bunch of WIP stuff that doesn't belong in the repo

--- patch/powerup_files/powerups/unimplemented/cat_suit/big_image_code.asm
+++
@@ -1,8 +0,0 @@
-;##################################################################################################
-;# Powerup: Big Mario
-;# Author: Nintendo
-;# Description: Default powerup $01 for Mario. Behaves exactly like the original game.
-;# 
-;# This code will handle any extra animations or poses Mario will have when using this powerup.
-
-	rts
--- patch/powerup_files/powerups/unimplemented/ice_flower/big_main_code.asm
+++
@@ -1,8 +0,0 @@
-;##################################################################################################
-;# Powerup: Big Mario
-;# Author: Nintendo
-;# Description: Default powerup $01 for Mario. Behaves exactly like the original game.
-;#
-;# This code is run every frame when Mario has this powerup.
-	
-	rts
